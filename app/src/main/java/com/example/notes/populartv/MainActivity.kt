1af58854fc0f060182fe13caf2db34b1b84804ef
Pavel Kotylo <pavelkotylo@gmail.com>
2021-05-05 00:41:15 +0300

    Add navigation_graph

---
+++ app/src/main/java/com/example/notes/populartv/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.notes.populartv
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
